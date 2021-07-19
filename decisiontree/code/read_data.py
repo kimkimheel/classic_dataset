ef89edad6874d56def4e68debd55d0860e15d56b
Kong.Lz <565150134@qq.com>
2021-07-19 11:02:24 +0800

    modify the problem in displaying pictures

--- decisiontree/code/read_data.py
+++ decisiontree/code/read_data.py
@@ -23,7 +23,7 @@ survived_count = outcomes.value_counts()  # !!! important
 
 fig1 = plt.figure(figsize=(6,6))
 plt.pie(list(survived_count),labels=['未存活','存活'],autopct='%1.1f%%',startangle=100)
-fig1.show()
+# fig1.show()
 
 # # 每一个特征对存活的影响
 # pclass
@@ -31,49 +31,52 @@ fig1.show()
 fig2 = plt.figure(figsize=(12,8))
 sns.set_theme(style='whitegrid')
 sns.countplot(x=full_data.Pclass, hue=full_data.Survived, palette='Blues')
-fig2.show()
+# fig2.show()
 
 # sex
 fig3 = plt.figure(figsize=(6,6))
 sns.set_theme(style='whitegrid')
 sns.countplot(x=full_data.Sex, hue=full_data.Survived, palette='Blues')
-fig3.show()
+# fig3.show()
 
 # Embarked
 fig4 = plt.figure(figsize=(6,6))
 sns.set_theme(style='whitegrid')
 sns.countplot(x=full_data.Embarked, hue=full_data.Survived, palette='Blues')
-fig4.show()
+# fig4.show()
 
 # age
 fig5 = plt.figure(figsize=(6,6))
 sns.set_theme(style='whitegrid')
 sns.boxplot(x=full_data.Survived, y = full_data.Age,palette='Blues')
-fig5.show()
+# fig5.show()
 
 # fare
 fig6 = plt.figure(figsize=(6,6))
 sns.set_theme(style='whitegrid')
 sns.violinplot(x=full_data.Survived, y = full_data.Fare,palette='Blues')
-fig6.show()
+# fig6.show()
 
 # sibsp
 full_data['sibsp'] = full_data['SibSp'].map(lambda x:1 if x else 0)
 fig7 = plt.figure(figsize=(6,6))
 sns.set_theme(style='whitegrid')
 sns.countplot(x=full_data.sibsp, hue=full_data.Survived, palette='Blues')
-fig7.show()
+# fig7.show()
 
 # parch
 full_data['parch'] = full_data['Parch'].map(lambda x:1 if x else 0)
 fig8 = plt.figure(figsize=(6,6))
 sns.set_theme(style='whitegrid')
 sns.countplot(x=full_data.parch, hue=full_data.Survived, palette='Blues')
-fig8.show()
+# fig8.show()
 
 # fellow
 full_data['fellow'] = full_data[['SibSp','Parch']].apply(lambda x:1 if x['SibSp'] or x['Parch'] else 0,axis=1)
 fig9 = plt.figure(figsize=(6,6))
 sns.set_theme(style='whitegrid')
 sns.countplot(x=full_data.fellow, hue=full_data.Survived, palette='Blues')
-fig9.show()
+# fig9.show()
+
+plt.show()
+# input()
